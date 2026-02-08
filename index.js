ab4e70557bd6117017928f7c5f01b352092f5e39
192020-stack <192020@cctt.edu.ly>
2026-02-08 09:15:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post("/webhook", async (req, res) => {
 
   try {
     // إرسال إلى زامات باستخدام التوكن
-    await axios.post('http://102.203.200.112/', {
+    await axios.post('http://102.203.200.112/#ticket/create/id/', {
       title: 'WhatsApp Ticket',
       article: {
         body: message,
